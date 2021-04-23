44f283aade18740c134256314fcc160eddd6d76e
John Barton <john@mediayoucanfeel.com>
2021-04-23 16:21:50 -0400

    Add initial styling to homepage

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from movie import Movie
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route("/")
 def index():
-    m = Movie.get_random()
-    html = f"""
-    <h1 style="text-align: center;">{m.title} ({m.year})</h1>
-    <hr>
-    <p style="text-align: center;">{m.plot}</p>
-    """
-    return html
+    # m = Movie.get_random()
+    # html = f"""
+    # <h1 style="text-align: center;">{m.title} ({m.year})</h1>
+    # <hr>
+    # <p style="text-align: center;">{m.plot}</p>
+    # """
+    # return html
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
