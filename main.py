8de7402d598acf4a7edd5538f995b9b6ec4f97f8
John Barton <john@mediayoucanfeel.com>
2021-04-23 22:45:21 -0400

    Initial socket implementation for joining rooms

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, make_response, render_template, request
+from flask_socketio import SocketIO, join_room, leave_room, send
 
 from movie import Movie
 from room import Room
 from user import User
 
 app = Flask(__name__, template_folder='templates')
+socketio = SocketIO(app)
 rooms = []
 
 
@@ -27,11 +29,14 @@ def new_room():
     user = User(request.cookies.get('user_name'))
     room = Room(user)
     rooms.append(room)
-    return render_template('room.html', room=room)
+
+    resp = make_response(render_template('room.html', room=room))
+    resp.set_cookie('room', room.code)
+    return resp
 
 
 @app.route('/joinRoom', methods=['POST'])
-def join_room():
+def join_malarkey_room():
     user = User(request.cookies.get('user_name'))
     form_data = request.form
     room_code = form_data['Room_Code']
@@ -39,10 +44,30 @@ def join_room():
     for room in rooms:
         if room.code == room_code:
             room.add_user(user)
-            return render_template('room.html', room=room)
+            resp = make_response(render_template('room.html', room=room))
+            resp.set_cookie('room', room.code)
+
+            return resp
 
     return f'<h1>No room {room_code} found!</h1>'
 
 
+@socketio.on('join')
+def connect(data):
+    user = User(request.cookies.get('user_name'))
+    room = None
+    for open_room in rooms:
+        print(open_room.code)
+        if open_room.code == request.cookies.get('room'):
+            room = open_room
+            break
+    print(f'{user.name} joined {room.code}')
+
+
+@socketio.on('disconnect')
+def disconnect():
+    print('Disconnected!')
+
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8010, debug=True)
+    socketio.run(app, host="127.0.0.1", port=8010, debug=True)
